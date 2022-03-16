52f9d0bfcbcbe5e2d9de534aaafbe2e928b19b37
ji <jiyongprk@gmail.com>
2022-03-16 17:35:13 -0400

    id and title to object, project title show on website

--- main.js
+++ main.js
@@ -1,48 +1,84 @@
-const createHTML = (()=>{
-    const container = document.querySelector('.container');
-    container.innerHTML = 
-    `
-    <!-- start -->
-    <header class="title">
-        <h1>My To-DO List</h1>
-    </header>
-    <div class="sidebar">
-        <h2>Projects</h2>
-        <p>Example Project</p>
-        <p>Example Project</p>
-        <p>Example Project</p>
-        <p>Example Project</p>
-        <p>Example Project</p>
-        <p>Example Project</p>
-        <button class="add-task-btn">ADD PROJECT</button>
-    </div>
-    <div class="content">
-        <!-- start -->
-        <div class="task">
-            <div class="task-name">
-                <p>Example testing</p>
-            </div>
-            <div class="details">
-                <button>details</button>
-            </div>
-            <div class="date">
-                <p>date</p>
-            </div>
-            <div class="btn-container">
-                <button class="edit">edit</button>
-                <button class="delete">delete</button>
-                <input type="checkbox">Complete</input>
-            </div>
-        </div>
-        <button class="new-task-btn">New Task</button>
-    </div>
-    <!-- end -->
-    `;
-});
+//object creation factory
+const projectNameFactory = (id, title, task) => {
+    return {id, title,task};
+};
 
-window.addEventListener('DOMContentLoaded',()=>{
-    createHTML();
-})
+const taskInfoFactory = (description, dueDate, priority) => {
+    return {description, dueDate, priority};
+};
 
+
+//project array
+let projectList = [];
+
+//html querySelectors
+const projectListContainer = document.querySelector('.project-list');
+const addProjectBtn = document.querySelector('.add-project-btn')
+const projectNameInput = document.querySelector('.project-name-input');
+
+//variables
+let ID = 0;
+
+
+//add project
+const addProject = () =>{
+    addProjectBtn.addEventListener('click',()=>{
+        
+        const newProjectName = projectNameFactory(ID++, projectNameInput.value);
+        projectList.push(newProjectName)
+
+        const listElement = document.createElement('li')
+        listElement.setAttribute('class', 'project-name');
+        projectListContainer.appendChild(listElement);
+        
+        selectProject(listElement);
+
+        for (let i = 0; i < projectList.length; i++){
+            listElement.textContent = projectList[i].title;
+        }
+
+        
+        console.log(projectList)
+    })
+};
+addProject();
+
+
+//html querySelectors
+const projectNameList = document.querySelectorAll('.project-name');
+
+//select project
+const selectProject = (elements) => {
+    elements.addEventListener('click',()=>{
+        console.log('clicked', projectList)
+        elements.style.color = "white";
+    })
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// start addTaskModule
+const newTaskBtn = document.querySelector('.new-task-btn');
+
+
+const addTask = () => {
+
+};
+//end addTaskModule
 
 
