1bcbf33e41ac04b8005110e17253f456472fac44
ji <jiyongprk@gmail.com>
2022-03-16 13:39:38 -0400

    createHTML module done

--- dist/main.js
+++ dist/main.js
@@ -1,26 +1,48 @@
-const container = document.querySelector('.container');
-container.innerHTML = 
-`
-<header class="title">
-    <h1>My To-DO List</h1></header>
-<div class="sidebar">
-    <h2>Projects</h2>
-    <p>Example Project</p>
-    <p>Example Project</p>
-    <p>Example Project</p>
-    <p>Example Project</p>
-    <p>Example Project</p>
-    <p>Example Project</p>
-    <button>ADD PROJECT</button>
-</div>
-<div class="content">
-    <p>testing</p>
-    <p>testing</p>
-    <p>testing</p>
-    <p>testing</p>
-    <button>New Task</button>
-</div>
-`;
+const createHTML = (()=>{
+    const container = document.querySelector('.container');
+    container.innerHTML = 
+    `
+    <!-- start -->
+    <header class="title">
+        <h1>My To-DO List</h1>
+    </header>
+    <div class="sidebar">
+        <h2>Projects</h2>
+        <p>Example Project</p>
+        <p>Example Project</p>
+        <p>Example Project</p>
+        <p>Example Project</p>
+        <p>Example Project</p>
+        <p>Example Project</p>
+        <button class="add-task-btn">ADD PROJECT</button>
+    </div>
+    <div class="content">
+        <!-- start -->
+        <div class="task">
+            <div class="task-name">
+                <p>Example testing</p>
+            </div>
+            <div class="details">
+                <button>details</button>
+            </div>
+            <div class="date">
+                <p>date</p>
+            </div>
+            <div class="btn-container">
+                <button class="edit">edit</button>
+                <button class="delete">delete</button>
+                <input type="checkbox">Complete</input>
+            </div>
+        </div>
+        <button class="new-task-btn">New Task</button>
+    </div>
+    <!-- end -->
+    `;
+});
+
+window.addEventListener('DOMContentLoaded',()=>{
+    createHTML();
+})
 
 
 
