fb95e1a734dce9bd6538593c376cae4e2405a78e
ji <jiyongprk@gmail.com>
2022-03-16 00:59:12 -0400

    work on javascript html

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("hello worddddd");
+const container = document.querySelector('.container');
+container.innerHTML = 
+`
+<header class="title">
+    <h1>My To-DO List</h1></header>
+<div class="sidebar">
+    <h2>Projects</h2>
+    <p>Example Project</p>
+    <p>Example Project</p>
+    <p>Example Project</p>
+    <p>Example Project</p>
+    <p>Example Project</p>
+    <p>Example Project</p>
+    <button>ADD PROJECT</button>
+</div>
+<div class="content">
+    <p>testing</p>
+    <p>testing</p>
+    <p>testing</p>
+    <p>testing</p>
+    <button>New Task</button>
+</div>
+`;
+
+
+
